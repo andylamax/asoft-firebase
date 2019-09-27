1f05e7036ddcb54ea54c4e1f4463e9431e3fea37
Anderson Lameck <andylamax@programmer.net>
2019-09-27 14:05:46 +0300

    Upgraded to asoft auth 7.0.0-alpha-2

--- src/commonMain/kotlin/tz/co/asoft/firebase/auth/dao/AuthFirebaseDao.kt
+++ src/commonMain/kotlin/tz/co/asoft/firebase/auth/dao/AuthFirebaseDao.kt
@@ -1,8 +1,11 @@
 package tz.co.asoft.firebase.auth.dao
 
 import tz.co.asoft.auth.User
+import tz.co.asoft.auth.dao.IAuthDao
 import tz.co.asoft.auth.exceptions.Exceptions
 import tz.co.asoft.firebase.core.FirebaseApp
+import tz.co.asoft.firebase.firestore.FirebaseFirestore
+import tz.co.asoft.firebase.firestore.dao.FirebaseDao
 import tz.co.asoft.firebase.firestore.firestore
 import tz.co.asoft.firebase.firestore.query.fetch
 import tz.co.asoft.firebase.firestore.query.where
@@ -10,22 +13,20 @@ import tz.co.asoft.firebase.firestore.snapshot.documents
 import tz.co.asoft.firebase.firestore.snapshot.toObject
 import tz.co.asoft.firebase.storage.*
 import tz.co.asoft.io.file.File
+import tz.co.asoft.persist.dao.Dao
 import tz.co.asoft.persist.tools.Cause
+import tz.co.asoft.persist.tools.Singleton
 
-class AuthFirebaseDao private constructor(val firebase: FirebaseApp) : AuthFirebaseAbstractDao(firebase.firestore()) {
-
+class AuthFirebaseDao private constructor(firestore: FirebaseFirestore, private val storage: FirebaseStorage) : FirebaseDao<User>(firestore, "users", User.serializer()), IAuthDao {
     companion object {
-        private var instance: AuthFirebaseAbstractDao? = null
-        fun getInstance(
-                firebase: FirebaseApp
-        ): AuthFirebaseAbstractDao = instance ?: AuthFirebaseDao(firebase).also {
-            instance = it
-        }
+        private var instance: Dao<User>? = null
+        fun getInstance(firestore: FirebaseFirestore, storage: FirebaseStorage) = instance
+                ?: AuthFirebaseDao(firestore, storage).also {
+                    instance = it
+                }
     }
 
-    val app by lazy { firebase }
-
-    private suspend fun signIn(method: String, key: String, pwd: String): User? {
+    suspend fun signIn(method: String, key: String, pwd: String): User? {
         val qs = collection.where("${method}s", "array-contains", key).fetch()
         val user = qs.documents.getOrNull(0)?.toObject(serializer) ?: throw Cause(Exceptions.UserNotFound.name)
         if (user.password == pwd) {
@@ -43,9 +44,9 @@ class AuthFirebaseDao private constructor(val firebase: FirebaseApp) : AuthFireb
         return signIn("phone", phone, pwd)
     }
 
-    override suspend fun uploadPhoto(user: User, file: File): User? {
-        val ref = app.storage().ref("user_profile_photos/${user.uid}")
-        val task = ref.upload(file)
+    override suspend fun uploadPhoto(user: User, photo: File): User? {
+        val ref = storage.ref("user_profile_photos/${user.uid}")
+        val task = ref.upload(photo)
         task.await()
         user.photoUrl = ref.downloadUrl() ?: return null
         return edit(user)
